80908f39e42112fa60c2c2637c67efb446a04e76
Diana <d7@hotmail.es>
2020-01-10 11:10:42 +0100

    separate constants in other file

---
+++ canvas-app/src/components/Board/constants.js
@@ -0,0 +1,13 @@
+const boxes = [
+  {index: 0, icon: 'all_inclusive', title: 'KEY PARTNERS', group: 'how'},
+  {index: 1, icon: 'work_outline', title: 'KEY ACTIVITIES', group: 'how'},
+  {index: 2, icon: 'location_city', title: 'KEY RESOURCES', group: 'how'},
+  {index: 3, icon: 'redeem', title: 'VALUE PROPOSITIONS', group: 'what'},
+  {index: 4, icon: 'group', title: 'CUSTOMER RELATIONSHIPS', group: 'with'},
+  {index: 5, icon: 'local_shipping', title: 'CHANNELS', group: 'with'},
+  {index: 6, icon: 'list_alt', title: 'CUSTOMER SEGMENTS', group: 'with'},
+  {index: 7, icon: 'money_off', title: 'COST STRUCTURE', group: 'how-many'},
+  {index: 8, icon: 'attach_money', title: 'REVENUE STREAMS', group: 'how-many'}
+]
+
+export default boxes
--- canvas-app/src/components/Board/index.js
+++ canvas-app/src/components/Board/index.js
@@ -2,18 +2,7 @@ import React, {useState, useEffect} from 'react'
 import './index.sass'
 import Header from '../Header'
 import Box from '../Box'
-
-const boxes = [
-  {index: 0, icon: 'all_inclusive', title: 'KEY PARTNERS', group: 'how'},
-  {index: 1, icon: 'work_outline', title: 'KEY ACTIVITIES', group: 'how'},
-  {index: 2, icon: 'location_city', title: 'KEY RESOURCES', group: 'how'},
-  {index: 3, icon: 'redeem', title: 'VALUE PROPOSITIONS', group: 'what'},
-  {index: 4, icon: 'group', title: 'CUSTOMER RELATIONSHIPS', group: 'with'},
-  {index: 5, icon: 'local_shipping', title: 'CHANNELS', group: 'with'},
-  {index: 6, icon: 'list_alt', title: 'CUSTOMER SEGMENTS', group: 'with'},
-  {index: 7, icon: 'money_off', title: 'COST STRUCTURE', group: 'how-many'},
-  {index: 8, icon: 'attach_money', title: 'REVENUE STREAMS', group: 'how-many'}
-]
+import boxes from './constants.js'
 
 export default function () {
 
