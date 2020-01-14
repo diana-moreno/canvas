f0a92c9a6f813e43969127686971af423c9adc07
Diana <d7@hotmail.es>
2020-01-14 21:57:05 +0100

    Updates

---
+++ precache-manifest.06c393bafec8db7a304075ccb7186c41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f180108193243af74459a238932e08bc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff0fed5243862d20b0b3",
+    "url": "./static/css/main.0ba25619.chunk.css"
+  },
+  {
+    "revision": "c077b578d7acc3735d04",
+    "url": "./static/js/2.2264ff59.chunk.js"
+  },
+  {
+    "revision": "ff0fed5243862d20b0b3",
+    "url": "./static/js/main.bd688f15.chunk.js"
+  },
+  {
+    "revision": "6a5f6f228c0713239c93",
+    "url": "./static/js/runtime-main.da0ad5c6.js"
+  }
+]);
